983986087f7381557674e8a3ff00789e13078742
jagdeesh1009 <jagdeesh1009@gmail.com>
2015-11-25 12:44:56 +0530

    simple case is added

--- src/test/java/com/sample/SearchTest.java
+++ src/test/java/com/sample/SearchTest.java
@@ -1,7 +1,12 @@
 package com.sample;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class SearchTest {
@@ -11,8 +16,20 @@ public class SearchTest {
 	
 		System.out.println("This is the sample test");
 		
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://www.subcontract.com");
+		WebDriver driver;
+		try {
+			driver = driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), DesiredCapabilities.firefox());
+			driver.get("http://www.subcontract.com");
+			
+			driver.quit();
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+				
+				//new FirefoxDriver();
+		
 		
 		//driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), DesiredCapabilities.firefox());
 			
