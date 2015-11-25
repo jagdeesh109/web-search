71ded27a29db724cd8956f8b4d161cdf4c81749a
jagdeesh1009 <jagdeesh1009@gmail.com>
2015-11-25 11:40:22 +0530

    remote webdriver is added

--- src/test/java/com/sample/SearchTest.java
+++ src/test/java/com/sample/SearchTest.java
@@ -1,7 +1,12 @@
 package com.sample;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class SearchTest {
@@ -11,8 +16,18 @@ public class SearchTest {
 	
 		System.out.println("This is the sample test");
 		
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://www.subcontract.com");
+		
+		WebDriver driver;
+		try {
+			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.firefox());
+			driver.get("http://www.subcontract.com");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		//WebDriver driver = new FirefoxDriver();
+		
 			
 	}
 }
