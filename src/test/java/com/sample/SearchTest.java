aafca79d789f034ecb3f2d479c494d7a11bbb084
jagdeesh1009 <jagdeesh1009@gmail.com>
2015-11-26 11:05:45 +0530

    mark the desired capablity of linux chrome

--- src/test/java/com/sample/SearchTest.java
+++ src/test/java/com/sample/SearchTest.java
@@ -3,6 +3,7 @@ package com.sample;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -19,7 +20,10 @@ public class SearchTest {
 		
 		WebDriver driver;
 		try {
+			DesiredCapabilities capabilities = new DesiredCapabilities("chrome", "46.0.2490.86",Platform.LINUX); 
+			
 			System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+			
 			driver = new RemoteWebDriver(new URL("http://172.16.30.61:4444/wd/hub"), DesiredCapabilities.chrome());
 			driver.get("http://www.subcontract.com");
 			
